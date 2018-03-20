6c7019af16ab6dc817724fe2ea949ba7689e3461
Prakash <SARP@EUR.AD.SAG>
2018-03-20 10:07:10 +0530

    Changes in location and device

--- plugins/parking-pi-core/Device/DeviceController.js
+++ plugins/parking-pi-core/Device/DeviceController.js
@@ -27,30 +27,40 @@
       var pos = device.c8y_Position;
       var details={
         serialNo: $scope.entire.length+1,
-       // lat: pos.lat,
-        //lng: pos.lng,
         name: device.name,
         deviceId:device.id
       };
+//want to check this
 
       var latlng = {lat: parseFloat(pos.lat), lng: parseFloat(pos.lng)};
       var geocoder = new google.maps.Geocoder;
       geocoder.geocode({'location': latlng}, function(results, status) {
-          if (status === 'OK' && results[0]) {
+          if (status === 'OK') {
+            if (results[0]) {
               console.log(results[0]);
+              //infowindow.setContent(results[0].formatted_address);
               details.location=results[0].formatted_address;
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
           }
         });
-
-      if (device.c8y_Connection) {
+//want to check the above code   
+      if (device.com_softwareag_parkingpi_ParkingPiStatus) {
+        //details.status = device.c8y_Connection.status;
         console.log(typeof(device.c8y_Connection.status));
         console.log(device.c8y_Connection.status);
-        if(device.c8y_Connection.status=="DISCONNECTED")
+        if(device.com_softwareag_parkingpi_ParkingPiStatus.status=="INACTIVE")
         {
           details.status = "fa-power-off text-danger";
         }
-        else{
+        else if(device.com_softwareag_parkingpi_ParkingPiStatus.status=="ACTIVE"){
           details.status = "fa-plug text-success";
+        }
+        else{
+          details.status = "fa-wrench text-warning";
         }
       }
       else{
--- plugins/parking-pi-core/Location/LocationController.js
+++ plugins/parking-pi-core/Location/LocationController.js
@@ -9,88 +9,69 @@
   function locationcontroller(
     $scope,
     $q,
-    c8yInventory,
-    c8yBinary
+    $routeParams,
+    c8yDevices,
+    c8yAlert
+
   ) {
-    $scope.markers = [];
 
-    var getDevicesAndBinaries = {
-      devices: getDevicesWithLocation(),
-      binaries: c8yBinary.list({})
-    };
-    $q.all(getDevicesAndBinaries).then(placeTypes);
+    c8yDevices.detail($routeParams.deviceId).then(function (res) {
+      var fulldata=res.data;
+      console.log("new location :",fulldata.c8y_Position);
+      var pos=fulldata.c8y_Position;
+      var details={};
 
-    function getDevicesWithLocation() {
-      var filters = {fragmentType: 'c8y_Position' };
-      return c8yInventory.list(filters);
-    }
 
-    function placeTypes(devicesAndBinaries) {
-      var devicesOfType = createTypeMap(devicesAndBinaries.devices);
-      var iconOfType = createIconMap(devicesAndBinaries.binaries);
-      angular.forEach(devicesOfType, _.curry(placeType)(iconOfType));
-    }
+      var latlng = {lat: parseFloat(pos.lat), lng: parseFloat(pos.lng)};
+      var geocoder = new google.maps.Geocoder;
+      geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[0]) {
+              console.log(results[0]);
+              //infowindow.setContent(results[0].formatted_address);
+              details.location=results[0].formatted_address;
+              console.log("location123",details.location);
 
-    function placeType(iconOfType, devices, type) {
-      var icon = iconOfType[type];
-      if (icon) {
-        var placeDevices = _.curry(place)(devices);
-        c8yBinary.downloadAsDataUri(icon).then(placeDevices);
-      } else {
-        place(devices);
-      }
-    }
+              console.log(results[0].formatted_address);
+              popupfunc(results[0].formatted_address);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
 
-    function createTypeMap(devices) {
-      var typeMap = {};
-      angular.forEach(devices, _.curry(addDeviceToTypeMap)(typeMap));
-      return typeMap;
-    }
 
-    function addDeviceToTypeMap(typeMap, device) {
-      var hw = 'default';
-      if (device.c8y_Hardware && device.c8y_Hardware.model) {
-        hw = device.c8y_Hardware.model;
-      }
 
-      if (!typeMap[hw]) {
-        typeMap[hw] = [];
-      }
+      popupfunc(address){
 
-      typeMap[hw].push(device);
-    }
+      var mymap = L.map('mapid').setView([pos.lat, pos.lng], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1Ijoic2FyYXZhbmFwcmFrYXNoIiwiYSI6ImNqZWk0cHl3NjA0OHcycG9zY2drNTh6NzYifQ.9k4pUoma8BUHyjyo1smidg', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'sk.eyJ1Ijoic2FyYXZhbmFwcmFrYXNoIiwiYSI6ImNqZWk0cHl3NjA0OHcycG9zY2drNTh6NzYifQ.9k4pUoma8BUHyjyo1smidg'
+}).addTo(mymap);
+    var greenIcon = L.icon({
+      iconUrl: 'parking_pi_parking-pi-core/Images/parking_marker_red.png',
+      shadowUrl: 'parking_pi_parking-pi-core/Images/marker-shadow.png',
 
-    function createIconMap(binaries) {
-      var iconMap = {};
-      angular.forEach(binaries, _.curry(addIconToIconMap)(iconMap));
-      return iconMap;
-    }
+      iconSize:     [58, 68], // size of the icon
+      shadowSize:   [18, 18], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
 
-    function addIconToIconMap(iconMap, icon) {
-      if (c8yBinary.isImage(icon)) {
-        var name = icon.name;
-        name = name.substring(0, name.lastIndexOf('.'));
-        iconMap[name] = icon;
-      }
-    }
+    var marker = L.marker([pos.lat, pos.lng],{icon: greenIcon}).addTo(mymap).bindPopup(address)
+    .openPopup();
 
-    function place(devices, uri) {
-      angular.forEach(devices, _.curry(placeDevice)(_, uri));
-    }
+    console.log("location",details.location);
+  }
 
-    function placeDevice(device, uri) {
-      var pos = device.c8y_Position;
-      var marker = {
-        lat: pos.lat,
-        lng: pos.lng,
-        message: '<a href="#/device/' + device.id + '">' + device.name + '</a>'
-      };
 
-      if (uri) {
-        marker.icon = { iconUrl: uri };
-      }
-
-      $scope.markers.push(marker);
-    }
+    });
+    
   }
 }());
